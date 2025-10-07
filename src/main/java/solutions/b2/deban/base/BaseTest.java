99fc0b2c80c17044b6fd63c31ca295a5a2245443
C1335230 Lucas de Oliveira Santana <c1335230@interno.bb.com.br>
2025-10-07 15:36:22 -0300

    Mudar nome do método anotado como BeforeAll

--- src/main/java/solutions/b2/deban/base/BaseTest.java
+++ src/main/java/solutions/b2/deban/base/BaseTest.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class BaseTest implements UtilsDados {
 
     @BeforeAll
-    public static void teardown() {
+    public static void setupRelatorioExcel() {
         gerarExcelExtracao();
     }
 
