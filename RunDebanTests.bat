5fa55ee236879c7016349894470ce87745cca78a
C1335230 Lucas de Oliveira Santana <c1335230@interno.bb.com.br>
2025-10-17 20:54:00 -0300

    altera caminho jar

--- RunDebanTests.bat
+++ RunDebanTests.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d "C:\Testes automatizados\DEBAN\DEBAN-TESTES-MAIN\out\artifacts\DEBAN_TESTES_MAIN_jar"
+cd /d "C:\Testes_automatizados\DEBAN\DEBAN-TESTES-MAIN\out\artifacts\DEBAN_TESTES_MAIN_jar"
 cmd.exe /k java -jar "DEBAN-TESTES-MAIN.jar"
