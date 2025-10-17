334f712cdc0ea6a7beedbaf5423543ca3e915738
C1335230 Lucas de Oliveira Santana <c1335230@interno.bb.com.br>
2025-10-17 20:52:13 -0300

    configura caminho base

--- src/main/java/solutions/b2/deban/base/ConfigCaminhoArquivos.java
+++ src/main/java/solutions/b2/deban/base/ConfigCaminhoArquivos.java
@@ -2,7 +2,13 @@ package solutions.b2.deban.base;
 
 public class ConfigCaminhoArquivos {
 
+    private static final String basePath = "C:\\Testes_automatizados\\DEBAN\\";
+
     public static String getCaminhoArquivos() {
-        return String.format("C:\\Testes automatizados\\DEBAN\\arquivos_deban\\%s\\%s\\", SetData.anoReferencia, SetData.trim);
+        return String.format(basePath+"arquivos_deban\\%s\\%s\\", SetData.anoReferencia, SetData.trim);
+    }
+
+    public static String getBasePath() {
+        return basePath;
     }
 }
