7562b06811513e36401d48082003238602befcc0
C1335230 Lucas de Oliveira Santana <c1335230@interno.bb.com.br>
2025-10-17 20:53:05 -0300

    altera caminho projeto e arredonda para duas casas decimais dados monetários

--- Filtragem_extracao.py
+++ Filtragem_extracao.py
@@ -1,27 +1,29 @@
 import os
 import pandas as pd
 
-extracao_caminho = r"C:\Testes automatizados\DEBAN\extracao\transacoes.csv"
+extracao_caminho = r"C:\Testes_automatizados\DEBAN\extracao\transacoes2.csv"
 saida_caminho = r"src\main\resources\transacoes_extraidas.xlsx"
 
 def retornar_soma_dados_excel(arquivo_entrada, coluna_filtro):
     # Lê o CSV
     df = pd.read_csv(arquivo_entrada, sep=",", decimal=".")
 
-    # # Filtro (opcional)
+    # Filtro (opcional)
     # if coluna_filtro in df.columns:
     #     df_filtrado = df[df[coluna_filtro] == 450]
     # else:
     df_filtrado = df
 
     # Colunas que queremos somar
-    colunas_para_somar = ["tarifa_intercambio", "soma_trn", "qtd_trn", "tarifa_desconto"]
+    colunas_para_somar = ["tarifa_de_intercambio", "b2c_t_trn_amount", "b2c_t_vlr_desconto"]
 
     # Calcula as somas apenas das colunas que existem no arquivo
     somas = {}
     for col in colunas_para_somar:
         if col in df_filtrado.columns:
-            somas[col] = df_filtrado[col].sum()
+            soma_col = df_filtrado[col].sum()
+            # Arredonda para duas casas decimais
+            somas[col] = round(soma_col, 2)
         else:
             somas[col] = None  # mantém chave para rastreabilidade
 
@@ -34,7 +36,7 @@ def retornar_soma_dados_excel(arquivo_entrada, coluna_filtro):
     # Gera o Excel com as somas
     df_resultado.to_excel(saida_caminho, index=False)
 
-    print(f"Arquivo de extração gerado com sucesso !")
+    print(f"Arquivo de extração gerado com sucesso!")
     return df_resultado
 
 if __name__ == "__main__":
